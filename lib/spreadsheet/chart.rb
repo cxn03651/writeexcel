ad06ba64bda90df86762b76f9a001cd691d2bb7d
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2009-03-04 21:27:17 +0900

    lib/spreadsheet/chart.rb:   start porting.

---
+++ lib/spreadsheet/chart.rb
@@ -0,0 +1,17 @@
+###############################################################################
+#
+# Chart - A writer class for Excel Charts.
+#
+#
+# Used in conjunction with Spreadsheet::WriteExcel
+#
+# Copyright 2000-2008, John McNamara, jmcnamara@cpan.org
+#
+# Documentation after __END__
+#
+
+require 'biffwriter'
+
+class Chart
+
+end
