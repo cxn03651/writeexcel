ec387f20750c009929475184303e3f261b2b3af2
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2009-03-16 22:05:59 +0900

    example/merge4.rb:   convert.
    test/tc_example_match.rb:
    test/perl_output/merge4.xls:
      add test_merge4

--- test/tc_example_match.rb
+++ test/tc_example_match.rb
@@ -630,6 +630,78 @@ class TC_example_match < Test::Unit::TestCase
     compare_file("perl_output/merge3.xls", @filename)
   end
 
+  def test_merge4
+    # Create a new workbook and add a worksheet
+    workbook  = Excel.new(@filename)
+    worksheet = workbook.add_worksheet
+    
+    # Increase the cell size of the merged cells to highlight the formatting.
+    (1..11).each { |row| worksheet.set_row(row, 30) }
+    worksheet.set_column('B:D', 20)
+    
+    ###############################################################################
+    #
+    # Example 1: Text centered vertically and horizontally
+    #
+    format1 = workbook.add_format(
+                                        :border  => 6,
+                                        :bold    => 1,
+                                        :color   => 'red',
+                                        :valign  => 'vcenter',
+                                        :align   => 'center'
+                                       )
+    
+    worksheet.merge_range('B2:D3', 'Vertical and horizontal', format1)
+    
+    
+    ###############################################################################
+    #
+    # Example 2: Text aligned to the top and left
+    #
+    format2 = workbook.add_format(
+                                        :border  => 6,
+                                        :bold    => 1,
+                                        :color   => 'red',
+                                        :valign  => 'top',
+                                        :align   => 'left'
+                                      )
+    
+    worksheet.merge_range('B5:D6', 'Aligned to the top and left', format2)
+    
+    ###############################################################################
+    #
+    # Example 3:  Text aligned to the bottom and right
+    #
+    format3 = workbook.add_format(
+                                        :border  => 6,
+                                        :bold    => 1,
+                                        :color   => 'red',
+                                        :valign  => 'bottom',
+                                        :align   => 'right'
+                                      )
+    
+    worksheet.merge_range('B8:D9', 'Aligned to the bottom and right', format3)
+    
+    ###############################################################################
+    #
+    # Example 4:  Text justified (i.e. wrapped) in the cell
+    #
+    format4 = workbook.add_format(
+                                        :border  => 6,
+                                        :bold    => 1,
+                                        :color   => 'red',
+                                        :valign  => 'top',
+                                        :align   => 'justify'
+                                      )
+    
+    worksheet.merge_range('B11:D12', 'Justified: '+'so on and '*18, format4)
+    
+    workbook.close
+
+    # do assertion
+    compare_file("perl_output/merge3.xls", @filename)
+  end
+
   def compare_file(expected, target)
     fh_e = File.open(expected, "r")
     fh_t = File.open(target, "r")
