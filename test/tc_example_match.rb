b651a37bec1c4ef2106e241a668aca138a655b7f
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2009-03-16 22:15:09 +0900

    examples/merge5.rb:   convert.
    test/tc_example_match.rb:
    test/perl_output/merge5.rb:
      add test_merge5

--- test/tc_example_match.rb
+++ test/tc_example_match.rb
@@ -702,6 +702,74 @@ class TC_example_match < Test::Unit::TestCase
     compare_file("perl_output/merge3.xls", @filename)
   end
 
+  def test_merge5
+    # Create a new workbook and add a worksheet
+    workbook  = Excel.new(@filename)
+    worksheet = workbook.add_worksheet
+    
+    
+    # Increase the cell size of the merged cells to highlight the formatting.
+    (3..8).each { |col| worksheet.set_row(col, 36) }
+    [1, 3, 5].each { |n| worksheet.set_column(n, n, 15) }
+    
+    
+    ###############################################################################
+    #
+    # Rotation 1, letters run from top to bottom
+    #
+    format1 = workbook.add_format(
+                                        :border      => 6,
+                                        :bold        => 1,
+                                        :color       => 'red',
+                                        :valign      => 'vcentre',
+                                        :align       => 'centre',
+                                        :rotation    => 270
+                                      )
+    
+    
+    worksheet.merge_range('B4:B9', 'Rotation 270', format1)
+    
+    
+    ###############################################################################
+    #
+    # Rotation 2, 90° anticlockwise
+    #
+    format2 = workbook.add_format(
+                                        :border      => 6,
+                                        :bold        => 1,
+                                        :color       => 'red',
+                                        :valign      => 'vcentre',
+                                        :align       => 'centre',
+                                        :rotation    => 90
+                                      )
+    
+    
+    worksheet.merge_range('D4:D9', 'Rotation 90°', format2)
+    
+    
+    
+    ###############################################################################
+    #
+    # Rotation 3, 90° clockwise
+    #
+    format3 = workbook.add_format(
+                                        :border      => 6,
+                                        :bold        => 1,
+                                        :color       => 'red',
+                                        :valign      => 'vcentre',
+                                        :align       => 'centre',
+                                        :rotation    => -90
+                                      )
+    
+    
+    worksheet.merge_range('F4:F9', 'Rotation -90°', format3)
+    
+    workbook.close
+    
+    # do assertion
+    compare_file("perl_output/merge3.xls", @filename)
+  end
+
   def compare_file(expected, target)
     fh_e = File.open(expected, "r")
     fh_t = File.open(target, "r")
