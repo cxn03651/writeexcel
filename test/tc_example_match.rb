2eccc42acfa7d79ea15f4f38fcc5c0a490d46e90
cxn03651 <cxn03651@msj.biglobe.ne.jp>
2009-03-18 11:40:38 +0900

    examples/date_time.rb:   converted.
    test/tc_example_match.rb:
    test/perl_output/date_time.xls
      add test_date_time.

--- test/tc_example_match.rb
+++ test/tc_example_match.rb
@@ -933,6 +933,82 @@ class TC_example_match < Test::Unit::TestCase
     compare_file("perl_output/protection.xls", @filename)
   end
 
+  def test_date_time
+    # Create a new workbook and add a worksheet
+    workbook  = Excel.new(@filename)
+    worksheet = workbook.add_worksheet
+    bold      = workbook.add_format(:bold => 1)
+    
+    # Expand the first column so that the date is visible.
+    worksheet.set_column("A:B", 30)
+    
+    # Write the column headers
+    worksheet.write('A1', 'Formatted date', bold)
+    worksheet.write('B1', 'Format',         bold)
+    
+    # Examples date and time formats. In the output file compare how changing
+    # the format codes change the appearance of the date.
+    #
+    date_formats = [
+        'dd/mm/yy',
+        'mm/dd/yy',
+        '',
+        'd mm yy',
+        'dd mm yy',
+        '',
+        'dd m yy',
+        'dd mm yy',
+        'dd mmm yy',
+        'dd mmmm yy',
+        '',
+        'dd mm y',
+        'dd mm yyy',
+        'dd mm yyyy',
+        '',
+        'd mmmm yyyy',
+        '',
+        'dd/mm/yy',
+        'dd/mm/yy hh:mm',
+        'dd/mm/yy hh:mm:ss',
+        'dd/mm/yy hh:mm:ss.000',
+        '',
+        'hh:mm',
+        'hh:mm:ss',
+        'hh:mm:ss.000',
+    ]
+    
+    # Write the same date and time using each of the above formats. The empty
+    # string formats create a blank line to make the example clearer.
+    #
+    row = 0
+    date_formats.each do |date_format|
+      row += 1
+      next if date_format == ''
+    
+      # Create a format for the date or time.
+      format =  workbook.add_format(
+                                  :num_format => date_format,
+                                  :align      => 'left'
+                                 )
+    
+      # Write the same date using different formats.
+      worksheet.write_date_time(row, 0, '2004-08-01T12:30:45.123', format)
+      worksheet.write(row, 1, date_format)
+    end
+    
+    # The following is an example of an invalid date. It is written as a string instead
+    # of a number. This is also Excel's default behaviour.
+    #
+    row += 2
+    worksheet.write_date_time(row, 0, '2004-13-01T12:30:45.123')
+    worksheet.write(row, 1, 'Invalid date. Written as string.', bold)
+    
+    workbook.close
+
+    # do assertion
+    compare_file("perl_output/date_time.xls", @filename)
+  end
+
   def compare_file(expected, target)
     fh_e = File.open(expected, "r")
     fh_t = File.open(target, "r")
