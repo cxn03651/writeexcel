e2fbafa10b72d73feddd5e99c57c6d72cd73d1d7
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2010-03-31 07:29:07 +0900

    chart_bar.rb converted. test added.

--- lib/writeexcel/charts/bar.rb
+++ lib/writeexcel/charts/bar.rb
@@ -47,5 +47,57 @@ class Chart
 
       append(header, data)
     end
+
+    ###############################################################################
+    #
+    # _store_x_axis_text_stream()
+    #
+    # Write the X-axis TEXT substream. Override the parent class because the axes
+    # are reversed.
+    #
+    def store_x_axis_text_stream
+      formula = @x_axis_formula.nil? ? '' : @x_axis_formula
+      ai_type = _formula_type(2, 1, formula)
+
+      store_text(0x002D, 0x06D9, 0x5F, 0x1CC, 0x0281, 0x00, 90)
+
+      store_begin
+      store_pos(2, 2, 0, 0, 0x17, 0x2A)
+      store_fontx(8)
+      store_ai(0, ai_type, formula)
+
+      unless @x_axis_formula.nil?
+        store_seriestext(@x_axis_name, @x_axis_encoding)
+      end
+
+      store_objectlink(3)
+      store_end
+    end
+
+    ###############################################################################
+    #
+    # _store_y_axis_text_stream()
+    #
+    # Write the X-axis TEXT substream. Override the parent class because the axes
+    # are reversed.
+    #
+    def store_y_axis_text_stream
+      formula = @y_axis_formula.nil? ? '' : @y_axis_formula
+      ai_type = _formula_type(2, 1, formula)
+
+      store_text(0x078A, 0x0DFC, 0x011D, 0x9C, 0x0081, 0x0000)
+
+      store_begin
+      store_pos(2, 2, 0, 0, 0x45, 0x17)
+      store_fontx(8)
+      store_ai(0, ai_type, formula)
+
+      unless @y_axis_formula.nil?
+        store_seriestext(@y_axis_name, @y_axis_encoding)
+      end
+
+      store_objectlink(2)
+      store_end
+    end
   end
 end
--- test/tc_example_match.rb
+++ test/tc_example_match.rb
@@ -1343,6 +1343,93 @@ class TC_example_match < Test::Unit::TestCase
     compare_file("#{PERL_OUTDIR}/chart_area.xls", @filename)
   end
 
+  def test_chart_bar
+    workbook  = WriteExcel.new(@filename)
+    worksheet = workbook.add_worksheet
+
+    # Add data to the worksheet that the charts willrefer to.
+    data = [
+        [ 'Category', 2, 3, 4, 5, 6, 7 ],
+        [ 'Values 1', 1, 4, 5, 2, 1, 5 ],
+        [ 'Values 2', 3, 6, 7, 5, 4, 3 ]
+    ]
+
+    worksheet.write('A1', data)
+
+
+    ###############################################################################
+    #
+    # Example 1. A minimal chart.
+    #
+    chart1 = workbook.add_chart(:name => 'Chart1', :type => Chart::Bar)
+
+    # Add values only. Use the default categories.
+    chart1.add_series( :values => '=Sheet1!$B$2:$B$7' )
+
+    ###############################################################################
+    #
+    # Example 2. A minimal chart with user specified categories (X axis)
+    #            and a series name.
+    #
+    chart2 = workbook.add_chart( :name => 'Chart2', :type => Chart::Bar )
+
+    # Configure the series.
+    chart2.add_series(
+        :categories => '=Sheet1!$A$2:$A$7',
+        :values     => '=Sheet1!$B$2:$B$7',
+        :name       => 'Test data series 1'
+    )
+
+    ###############################################################################
+    #
+    # Example 3. Same as previous chart but with added title and axes labels.
+    #
+    chart3 = workbook.add_chart( :name => 'Chart3', :type => Chart::Bar )
+
+    # Configure the series.
+    chart3.add_series(
+        :categories => '=Sheet1!$A$2:$A$7',
+        :values     => '=Sheet1!$B$2:$B$7',
+        :name       => 'Test data series 1'
+    )
+
+    # Add some labels.
+    chart3.set_title( :name => 'Results of sample analysis' )
+    chart3.set_x_axis( :name => 'Sample number' )
+    chart3.set_y_axis( :name => 'Sample length (cm)' )
+
+    ###############################################################################
+    #
+    # Example 4. Same as previous chart but with an added series
+    #
+    chart4 = workbook.add_chart( :name => 'Chart4', :type => Chart::Bar )
+
+    # Configure the series.
+    chart4.add_series(
+        :categories => '=Sheet1!$A$2:$A$7',
+        :values     => '=Sheet1!$B$2:$B$7',
+        :name       => 'Test data series 1'
+    )
+
+    # Add another series.
+    chart4.add_series(
+        :categories => '=Sheet1!$A$2:$A$7',
+        :values     => '=Sheet1!$C$2:$C$7',
+        :name       => 'Test data series 2'
+    )
+
+    # Add some labels.
+    chart4.set_title( :name => 'Results of sample analysis' )
+    chart4.set_x_axis( :name => 'Sample number' )
+    chart4.set_y_axis( :name => 'Sample length (cm)' )
+
+    # File save
+    workbook.close
+
+    # do assertion
+    compare_file("#{PERL_OUTDIR}/chart_bar.xls", @filename)
+  end
+
   def compare_file(expected, target)
     fh_e = File.open(expected, "r")
     fh_t = File.open(target, "r")
