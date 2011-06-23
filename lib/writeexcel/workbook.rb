f195bb8bab0691e62b4edc180028813ae9e60cfb
Hideo Nakamura <cxn03651@msj.biglobe.ne.jp>
2011-06-23 15:23:59 +0900

    * delete unneccessary require file.

--- lib/writeexcel/workbook.rb
+++ lib/writeexcel/workbook.rb
@@ -11,7 +11,6 @@
 # original written in Perl by John McNamara
 # converted to Ruby by Hideo Nakamura, cxn03651@msj.biglobe.ne.jp
 #
-require 'digest/md5'
 require 'nkf'
 require 'writeexcel/biffwriter'
 require 'writeexcel/worksheet'
