f69a622b715f94d6e9779e1aea44ef886dcccb71
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2011-08-12 21:26:47 +0900

    * add embedded_chart.rb

--- lib/writeexcel/worksheet.rb
+++ lib/writeexcel/worksheet.rb
@@ -17,6 +17,7 @@ require 'writeexcel/formula'
 require 'writeexcel/compatibility'
 require 'writeexcel/image'
 require 'writeexcel/cell_range'
+require 'writeexcel/embedded_chart'
 
 class MaxSizeError < StandardError   #:nodoc:
 end
@@ -320,34 +321,6 @@ class Worksheet < BIFFWriter
       grbit |= level << 8
       grbit |= 0x1000 if @collapsed && @collapsed != 0
       grbit
-    end
-  end
-
-  class EmbeddedChart
-    attr_reader :row, :col, :chart, :vertices
-
-    def initialize(worksheet, row, col, chart, x_offset = 0, y_offset = 0, scale_x = 1, scale_y = 1)
-      @worksheet = worksheet
-      @row, @col, @chart, @x_offset, @y_offset, @scale_x, @scale_y =
-        row, col, chart, x_offset, y_offset, scale_x, scale_y
-      @width = default_width * scale_x
-      @height = default_height * scale_y
-      @vertices = calc_vertices
-    end
-
-    # Calculate the positions of comment object.
-    def calc_vertices
-      @worksheet.position_object( @col, @row, @x_offset, @y_offset, @width, @height)
-    end
-
-    private
-
-    def default_width
-      526
-    end
-
-    def default_height
-      319
     end
   end
 
