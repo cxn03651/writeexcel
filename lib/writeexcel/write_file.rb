8d408c6c104479bf1050f568d9fb1f41fc476d2b
Hideo NAKAMURA <cxn03651@msj.biglobe.ne.jp>
2011-08-17 17:36:07 +0900

    * add private method WriteFile#join_data

--- lib/writeexcel/write_file.rb
+++ lib/writeexcel/write_file.rb
@@ -8,13 +8,8 @@ class WriteFile
   # General storage function
   #
   def prepend(*args)
-    data =
-      ruby_18 { args.join } ||
-      ruby_19 { args.collect{ |arg| arg.dup.force_encoding('ASCII-8BIT') }.join }
-    data = add_continue(data) if data.bytesize > @limit
-
-    @datasize += data.bytesize
-    @data      = data + @data
+    data = join_data(args)
+    @data = data + @data
 
     data
   end
@@ -26,18 +21,27 @@ class WriteFile
   # General storage function
   #
   def append(*args)
+    data = join_data(args)
+
+    if @using_tmpfile
+      @filehandle.write(data)
+    else
+      @data += data
+    end
+
+    data
+  end
+
+  private
+
+  def join_data(args)
     data =
       ruby_18 { args.join } ||
       ruby_19 { args.collect{ |arg| arg.dup.force_encoding('ASCII-8BIT') }.join }
     # Add CONTINUE records if necessary
     data = add_continue(data) if data.bytesize > @limit
-    if @using_tmpfile
-      @filehandle.write data
-      @datasize += data.bytesize
-    else
-      @datasize += data.bytesize
-      @data      = @data + data
-    end
+
+    @datasize += data.bytesize
 
     data
   end
